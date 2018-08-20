5dc51b1894617738d4e99d5dcdee8e724ad290c3
Onur Yıldırım <onur@cutepilot.com>
2018-08-21 02:51:46 +0300

    minor

--- test/index.js
+++ test/index.js
@@ -9,19 +9,22 @@ const jasmine = new Jasmine();
 jasmine.loadConfig(jasmineConfig);
 jasmine.jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000;
 
+console.log('');
+console.log('--------------------------------------------------');
+
 // setup reporter
 const reporterConfigFile = process.argv[2] || './config/reporter.config-1.json';
+console.log('DEVELOPMENT TEST:', reporterConfigFile);
+console.log('--------------------------------------------------');
 const reporterConfig = require(reporterConfigFile);
+console.log('original config:', JSON.stringify(reporterConfig));
 const reporter = new JasmineConsoleReporter(reporterConfig);
 jasmine.env.clearReporters();
 jasmine.addReporter(reporter);
 
 // run
-console.log('');
 console.log('--------------------------------------------------');
-console.log('DEVELOPMENT TEST:', reporterConfigFile);
-console.log('--------------------------------------------------');
-console.log('config:', JSON.stringify(reporterConfig, null, 2));
+console.log('config:', JSON.stringify(reporter.options));
 console.log('--------------------------------------------------');
 console.log('');
 jasmine.execute();
